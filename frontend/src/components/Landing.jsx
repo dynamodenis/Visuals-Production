6034c1912ae7cc05be3d2109e16564935ef81e42
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-02-11 19:33:54 +0300

    fixing landing

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Camera, Video, Mic, Laptop, ChevronRight } from 'lucide-react';
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 
 
 const Landing = () => {
   // Text to be animated
-  const text = "Welcome to Visuals Production";
+  const text = "Welcome to Visuals Production.";
+
+  const our_work_array = [
+    "Producing marketing videos, event videos, and social media content.",
+    "Managing paid advertising on platforms like Google Ads and Facebook Ads to drive traffic and sales.",
+    "High-end production gear rental for your creative projects.",
+    "Creating and managing social media accounts for businesses.",
+  ]
 
   // Variants for the container and individual letters
   const containerVariants = {
@@ -21,6 +28,33 @@ const Landing = () => {
   const letterVariants = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0 },
+  };
+
+  // Variants for the list items
+  // Variants for the list items
+  const listVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delayChildren: 0.6, // Delay for the first list item
+        staggerChildren: 0.4, // Delay between each list item
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: -20 },
+    visible: { 
+      opacity: 1, 
+      x: 0,
+      transition: {
+        duration: 0.8,
+        type: "spring",
+        damping: 10
+      }
+    }
   };
 
   return (
@@ -41,9 +75,26 @@ const Landing = () => {
                 </motion.span>
               ))}
             </motion.h2>
-            <p className="text-xl mb-8">High-end production gear rental for your creative projects</p>
+            {/* <p className="text-xl mb-8">High-end production gear rental for your creative projects</p> */}
+            <motion.ul
+              className='md:pl-10'
+              variants={listVariants}
+              initial="hidden"
+              animate="visible"
+            >
+              {our_work_array.map((item, index) => (
+                <motion.li 
+                  key={index} 
+                  variants={itemVariants}
+                  className="flex items-center p-4"
+                  >
+                  <ChevronRight className="w-5 h-5 text-blue-500 mr-2" />
+                  <span>{item}</span>
+                </motion.li>
+              ))}
+            </motion.ul>
             <button className="bg-red-600 hover:bg-red-700 px-8 py-3 rounded-lg font-semibold flex items-center">
-              Browse Equipment
+              Equipment Marketplace
               <ChevronRight className="ml-2" />
             </button>
           </div>
