b41970e5ef8079e469ca543edb3aa91e1d2dcc84
Denis Mbugua <60645043+dynamodenis@users.noreply.github.com>
2025-02-20 19:29:58 +0300

    details

--- frontend/src/pages/EquipmentDetail.jsx
+++ frontend/src/pages/EquipmentDetail.jsx
@@ -22,7 +22,7 @@ const EquipmentDetail = () => {
     return (
         <div className="container mx-auto px-6 pt-8 md:py-12 max-w-6xl">
             <div className='flex flex-col items-center pt-10 md:py-10'>
-                <h2 className="text-3xl font-medium font-bold mb-6 text-center flex flex-row justify-center items-center text-gray-900"><Video className="mr-3 text-red-500" /><span>{product.name} </span><Mic className="ml-2 text-red-500" /></h2>
+                <h2 className="text-3xl font-medium font-bold mb-6 text-center flex flex-row justify-center items-center text-gray-900"><Video className="mr-3 text-red-400" /><span>{product.name} </span><Mic className="ml-2 text-red-400" /></h2>
             </div>
             {/* Product Image */}
             <div className="flex flex-col md:flex-row w-full gap-4 md:gap-10">
@@ -38,7 +38,7 @@ const EquipmentDetail = () => {
                     <div className="flex flex-col items-start">
                         <div className="text-xl font-bold text-red-400">{product.name}</div>
 
-                        <div className="mb-4">
+                        <div className="mb-2">
                             <span className="text-md text-green-600 font-semibold font-small">
                                 {product.currency}{product.price.toLocaleString()}
                             </span>
@@ -46,25 +46,23 @@ const EquipmentDetail = () => {
                     </div>
 
                     {/* Quantity and Add to Cart */}
-                    <div className="flex gap-4 mb-6">
+                    <div className="flex gap-4 mb-4">
                         <div className="flex border rounded">
-                            <button className="px-4 py-2 border-r hover:bg-gray-100">-</button>
                             <input
                                 type="number"
-                                className="w-16 text-center"
+                                className="border rounded border-gray-300  focus:outline-none focus:ring-1 focus:ring-gray-600 focus:border-transparent w-10"
                                 min="1"
                                 defaultValue="1"
                             />
-                            <button className="px-4 py-2 border-l hover:bg-gray-100">+</button>
                         </div>
-                        <button className="bg-green-500 text-white px-6 py-2 rounded flex items-center gap-2 hover:bg-green-600">
-                            <ShoppingCart size={20} />
+                        <button className="bg-green-500 text-sm  text-white px-2 py-2 rounded flex items-center gap-2 hover:bg-green-600">
+                            <ShoppingCart size={15} />
                             Add to cart
                         </button>
                     </div>
 
                     {/* Categories */}
-                    <div className="mb-6">
+                    <div className="mb-2 text-sm">
                         <span className="text-gray-600">Categories: </span>
                         {product.categories.map((category, index) => (
                             <span key={category}>
@@ -75,30 +73,38 @@ const EquipmentDetail = () => {
                     </div>
 
                     {/* Social Share */}
-                    <div className="flex gap-4 mb-8">
+                    <div className="flex gap-2 mb-8 text-sm">
                         <button className="flex items-center gap-2 text-gray-600 hover:text-gray-800">
-                            <Share size={20} /> Share
+                            <Share size={15} /> Share
                         </button>
                         <button className="flex items-center gap-2 text-gray-600 hover:text-gray-800">
-                            <Heart size={20} /> Wishlist
+                            <Heart size={15} /> Wishlist
                         </button>
                     </div>
                 </div>
             </div>
 
             {/* Product Details */}
-            <div className="w-full md:w-1/2 p-4">
-
-
+            <div className="w-full ">
                 {/* Key Features */}
-                <div>
-                    <h2 className="text-xl font-semibold mb-4">Key Features</h2>
-                    <ul className="list-disc pl-5 space-y-2">
-                        {product.features.map((feature) => (
-                            <li key={feature} className="text-gray-700">{feature}</li>
-                        ))}
-                    </ul>
+                <div className='flex flex-col items-start pt-10'>
+                    <div className="text-xl font-semibold text-gray-600 underline underline-offset-6 decoration-red-400 ">Description</div>
+                    <div className="w-full border-t border-gray-300 mb-2"></div>
+                    <div className='flex flex-col items-start justify-start font-small text-sm md:w-1/2'>
+                        <div className="font-semibold mb-2 text-gray-400 ">Key Features</div>
+                        <ul className="list-disc pl-4 ml-8">
+                            {product.features.map((feature) => (
+                                <li key={feature} className="text-gray-400">{feature}</li>
+                            ))}
+                        </ul>
+                    </div>
+
                 </div>
+            </div>
+
+            <div className='flex flex-col items-start md:py-10'>
+                <div className="text-xl font-semibold mb-4 text-gray-600">Related Products</div>
+
             </div>
         </div>
     );
