e5f95fe47901b5489e902f1c39144f27d1bb56ad
Dynamo Denis <dynamodenis@MacBook-Pro.local>
2025-06-06 13:59:03 +0300

    added kesh

--- frontend/src/zustand/store.ts
+++ frontend/src/zustand/store.ts
@@ -1,15 +1,5 @@
 import { create } from 'zustand';
 
-// import a7_iv from '../assets/sony_a7_iv.webp';
-// import sigma_35mm_14 from '../assets/sigma_35mm_14.jpg'
-// import dji_mic_mini from '../assets/dji_mic_mini.webp'
-// import dji_mic_m2 from '../assets/dji_mic_m2.webp'
-// import rs4_pro from '../assets/rs4_pro.webp'
-// import amaran_300c from '../assets/amaram_300c.jpg'
-// import softbox from '../assets/softbox.jpg'
-// import insta360 from '../assets/insta360.webp'
-
-
 type Equipement = {
   title: string;
   category: string[];
@@ -37,48 +27,63 @@ export const useEquipementStore = create<EquipementStore>((set) => ({
           title: "Sony a7 IV",
           category: ["Camera"],
           price: 3000,
+          currency: "KSH",
           image: '/photos/sony_a7_iv.webp'
         },
         {
           title: "Sigma 35mm f/1.4",
           category: ["Lens", "Camera"],
           price: 3000,
+          currency: "KSH",
+
           image: '/photos/sigma_35mm_14.jpg'
         },
         {
           title: "DJI Mic Mini Wireless Microphone",
           category: ["Audio"],
           price: 1000,
+          currency: "KSH",
+
           image: '/photos/dji_mic_mini.webp'
         },
         {
           title: "DJI Mic M2 Wireless Microphone",
           category: ["Audio"],
           price: 1000,
+          currency: "KSH",
+
           image: '/photos/dji_mic_m2.webp'
         },
         {
           title: "RS4 Pro Gimbal",
           category: ["Stabilizer", "Camera"],
           price: 2000,
+          currency: "KSH",
+
           image: '/photos/rs4_pro.webp'
         },
         {
           title: "Aputure Amaran 300C RGB LED Light",
           category: ["Lighting"],
           price: 1000,
+          currency: "KSH",
+
           image: '/photos/amaram_300c.jpg'
         },
         {
           title: "Softbox with Grid (85cm)",
           category: ["Lighting"],
           price: 500,
+          currency: "KSH",
+
           image: '/photos/softbox.jpg'
         },
         {
           title: "Insta360 X4 Action Camera",
           category: ["Camera"],
           price: 1000,
+          currency: "KSH",
+
           image: '/photos/insta360.webp'
         }
       ];
